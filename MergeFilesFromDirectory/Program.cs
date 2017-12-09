963dd4b5f04218b5a81ac7dc766e1e74b8a5bf0c
Gustavo Morales <gmorales@selarom.us>
2017-12-09 11:01:49 -0800

    Add project files.

---
+++ MergeFilesFromDirectory/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace MergeFilesFromDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] txtFiles;
+            string txtFileDestination = @"C:\PSWA\Stop Loss\QP\Files\";
+            txtFiles = Directory.GetFiles( txtFileDestination, "*.txt");
+            using (StreamWriter writer = new StreamWriter(txtFileDestination + @"\allfiles.txt"))
+            {
+                for (int i = 0; i < txtFiles.Length; i++)
+                {
+                    using (StreamReader reader = File.OpenText(txtFiles[i]))
+                    {
+                        writer.Write(reader.ReadToEnd());
+                    }
+                }
+            }
+        }
+    }
+}
